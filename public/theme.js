208737a1d1b9526f927e84a77f93c5074332dea3
Saleel <saleel97@gmail.com>
2019-11-02 01:39:13 +0400

    Fix css

--- public/theme.js
+++ public/theme.js
@@ -1,4 +1,9 @@
-const lastTheme = window.localStorage.getItem('theme') || 'image';
+let lastTheme = window.localStorage.getItem('theme');
+
+if (['light', 'dark'].includes(lastTheme)) {
+  lastTheme = 'image';
+}
+
 if (lastTheme) {
   document.documentElement.className = lastTheme;
 }
