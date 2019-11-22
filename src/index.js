6e2795f8f21dd41c251ed6b5d1c24049f6e1f73e
Saleel <saleel97@gmail.com>
2019-11-22 18:49:17 +0400

    Add service worker for web app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './styles/main.scss';
 import 'react-tippy/dist/tippy.css';
 import App from './app';
+import * as serviceWorker from './serviceWorker';
+import { isWebApp } from './constants';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (isWebApp) {
+  serviceWorker.register();
+} else {
+  serviceWorker.unregister();
+}
